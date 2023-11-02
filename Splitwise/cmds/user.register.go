41be2ec4b2968dfde6bb738dc8954d729e37c8a3
Akhilesh Mahajan <Akhilesh.Mahajan@airtel.com>
2023-11-02 16:19:15 +0530

    updated: added command registry

--- Splitwise/cmds/user.register.go
+++ Splitwise/cmds/user.register.go
@@ -14,12 +14,18 @@ func NewUserRegisterCommand() ICommmand {
 
 func (urc *userRegisterCoomand) Execute() {
 	fmt.Println("User Register Command Executed")
+	//todo: add respective func to execute
 }
 
 // Example: RegisterUser <name> <phone number> <password>
 func (urc *userRegisterCoomand) Parse(command string) bool {
 	commandTokens := strings.Split(command, " ")
-	if len(commandTokens) != 4 || !strings.EqualFold(commandTokens[0], USER_REGISTER_COMMAND) || len(commandTokens[1]) == 0 || len(commandTokens[2]) == 0 || len(commandTokens[3]) == 0 {
+	if len(commandTokens) != 4 ||
+		!strings.EqualFold(commandTokens[0], USER_REGISTER_COMMAND) ||
+		len(commandTokens[1]) == 0 ||
+		len(commandTokens[2]) == 0 ||
+		len(commandTokens[3]) == 0 {
+
 		fmt.Println("Invalid Command")
 		return false
 	}
